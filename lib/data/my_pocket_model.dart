e6405aaaa3f7400bbe8e91bd1227c668e9ef862f
Eldis Simone <sayapakailinuxpak@gmail.com>
2025-01-15 23:51:42 +0700

    create Pocket class

--- lib/data/my_pocket_model.dart
+++ lib/data/my_pocket_model.dart
@@ -15,17 +15,17 @@ class MyPocketModel {
       pocketTitle: "Saving Balance", 
       remainingBalance: "\$ 1000.00"
     ),
-      MyPocketModel(
+    MyPocketModel(
       cardImageResource: "assets/images/pocket-credit-card-2-image.png", 
       pocketTitle: "Family Balance", 
       remainingBalance: "\$ 1500.00"
     ),
-      MyPocketModel(
+    MyPocketModel(
       cardImageResource: "assets/images/pocket-credit-card-3-image.png", 
       pocketTitle: "Investment Balance", 
       remainingBalance: "\$ 10000.00"
     ),
-      MyPocketModel(
+    MyPocketModel(
       cardImageResource: "assets/images/pocket-credit-card-4-image.png", 
       pocketTitle: "Alms Balance", 
       remainingBalance: "\$ 500.00"
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:dragonfly/screens/home.dart';
 import 'package:dragonfly/screens/notification_detail.dart';
 import 'package:dragonfly/screens/onboarding.dart';
 import 'package:dragonfly/screens/inbox.dart';
+import 'package:dragonfly/screens/pocket.dart';
 import 'package:dragonfly/screens/profile.dart';
 import 'package:flutter/material.dart';
 
@@ -25,13 +26,7 @@ class MyApp extends StatelessWidget {
         // the application has a purple toolbar. Then, without quitting the app,
         // try changing the seedColor in the colorScheme below to Colors.green
         // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
+        // reload" buttoncontext
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
         colorScheme: const ColorScheme.light(
@@ -104,7 +99,7 @@ class MyApp extends StatelessWidget {
         ),
         // useMaterial3: true,
       ),
-      home: Profile()
+      home: Pocket()
     );
   }
 }
